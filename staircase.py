9024b55404b68c9833f33a06a04af23d69f5218d
Sim Weinberg <simweinberg72@gmail.com>
2023-02-07 21:48:32 -0500

    stiarcase

---
+++ staircase.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    i = 1
+    spaces = n-1
+    multiplier = 1
+    while i < n:
+        print(" "*spaces + "#"*multiplier)
+        spaces = spaces - 1
+        multiplier = multiplier + 1
+        i += 1
+    print("#"*n)
+    
+    
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
