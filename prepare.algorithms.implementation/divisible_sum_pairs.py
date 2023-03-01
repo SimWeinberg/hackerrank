a6693ce73cf95404dc6f27b49f4e8890abd143be
Sim Weinberg <simweinberg72@gmail.com>
2023-02-28 22:14:15 -0500

    divisible sum pairs

---
+++ prepare.algorithms.implementation/divisible_sum_pairs.py
@@ -0,0 +1,10 @@
+# Given an array of integers and a positive integer k, determine the number of (i,j) pairs where i<j and ar[i] + ar[j] is divisible by k.
+
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(len(ar)):
+        for j in range(i+1,len(ar)):
+            if((ar[i]+ar[j])%k==0):
+                count += 1
+    return(count)
+            
